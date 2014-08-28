832799cbbda6c6a49c6fc476d31a85035e51bc0d
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-28 10:58:39 +1000

    data exploration

--- ppv-reg.R
+++ ppv-reg.R
@@ -5,7 +5,7 @@ top.1000.sites <- read.csv('data/top_1000_sites.tsv',sep='\t',stringsAsFactors=F
 str(top.1000.sites)
 head(top.1000.sites)
 png('PageViews_UniView.png')
-ggplot(top.1000.sites,aes(log(PageViews), log(UniqueVisitors))) + geom_point() +
+ggplot(top.1000.sites,aes(y=log(PageViews), x=log(UniqueVisitors))) + geom_point() +
     geom_smooth(method='lm',se=F)
 dev.off()
 ggplot(top.1000.sites,aes(log(PageViews))) + geom_density()
